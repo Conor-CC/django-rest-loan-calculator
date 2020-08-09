5558ad34af3305898746f596e57d0a40e9a2fbfb
ConorClery <cleryc@tcd.ie>
2020-08-09 17:18:22 +0100

    Admin site configurations

--- cflow/loanCalc/admin.py
+++ cflow/loanCalc/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from cflow.loanCalc.models import MonthlyRepaymentCalc, RepaymentCountCalc
 
-# Register your models here.
+
+@admin.register(MonthlyRepaymentCalc)
+class MonthlyRepaymentCalcAdmin(admin.ModelAdmin):
+    list_display = ('id', 'loan_amount', 'no_repayments',
+                    'monthly_repayment_amount', 'created')
+
+
+@admin.register(RepaymentCountCalc)
+class RepaymentCountCalcAdmin(admin.ModelAdmin):
+    list_display = ('id', 'loan_amount', 'monthly_repayment_amount',
+                    'no_repayments', 'created')
