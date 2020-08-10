62bb78538f6488de0049636e88dcbe18f6f5b817
ConorClery <cleryc@tcd.ie>
2020-08-10 19:41:52 +0100

    Added new calculator configurations

--- cflow/loanCalc/config.py
+++ cflow/loanCalc/config.py
@@ -4,11 +4,14 @@ Configuration file to specify parameters of the loanCalc application.
 CalculationSettings = {
     'monthly_repayment_calc': {
         'save_results': True,
+        'annual_interest_rate': 0.07
     },
     'repayment_count_calc': {
         'save_results': True,
+        'annual_interest_rate': 0.07
     },
-    'interest_rate_threshold_calc': {
+    'interest_rate_calc': {
         'save_results': True,
+        'threshold': 0.05
     }
 }
