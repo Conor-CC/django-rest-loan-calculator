72b27d3bdb4e0705f12c890a4b2efef2fddf0e06
ConorClery <cleryc@tcd.ie>
2020-08-11 23:11:30 +0100

    Set 'save_results' to False by default for all endpoints

--- cflow/loanCalc/config.py
+++ cflow/loanCalc/config.py
@@ -3,15 +3,15 @@ Configuration file to specify parameters of the loanCalc application.
 """
 CalculationSettings = {
     'monthly_repayment_calc': {
-        'save_results': True,
+        'save_results': False,
         'annual_interest_rate': 0.07
     },
     'repayment_count_calc': {
-        'save_results': True,
+        'save_results': False,
         'annual_interest_rate': 0.07
     },
     'interest_rate_calc': {
-        'save_results': True,
+        'save_results': False,
         'threshold': 0.05
     }
 }
